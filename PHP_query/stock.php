e82cb6329bb507f58a55305d0e6a984f243129df
skycckk <csie.chung@gmail.com>
2018-04-06 16:49:58 -0700

    add a portal for the stock search. ready to get stock info by using php HTTP methods

---
+++ PHP_query/stock.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* Created by Wei-Chung Huang.
+* Date: 4/6/18
+*/
+?>
+<html>
+<body>
+<head>
+    <style type="text/css">
+        div.box {
+            background-color: aliceblue;
+            text-align: center;
+            border-style: solid;
+            border-color: gray;
+            border-width: 1px;
+            width: 50%;
+            margin-left: 25%;
+        }
+    </style>
+</head>
+<div class="box">
+    <i>Stock Search</i><hr>
+    <div class="form">
+        <form action="" method="get">
+            Company Name of Symbol: <input type="text"><br>
+            <input type="submit" name="search" value="search">
+            <input type="submit" name="clear" value="clear">
+        </form>
+        <a href="https://ihsmarkit.com/products/digital.html"><p>Powered by Markit on Demand</p></a>
+    </div>
+</div>
+
+</body>
+</html>
